cc74e89cd1391a46eea01ed42cb6e53704497c72
CHEETAH <davesmith1095@gmail.com>
2026-01-26 00:47:15 +0000

    Final updates before assignment due

--- week02/exercise02.sql
+++ week02/exercise02.sql
@@ -1,5 +1,3 @@
-
-
 -- Use CTE to create birth year and later run expression on it
 WITH YEAR_FIX AS(
     SELECT *,
@@ -27,4 +25,7 @@ SELECT *,
 FROM YEAR_FIX
 
 
-SELECT COUNT(*) FROM output_zips
+/**
+
+This assignment reiterated a few important lessons in data science and problem solving. Primarily, I found it was important to keep myself grounded to the purpose of the assigment (denoting data quality issues) and that this is data represents something in the real world. It's easy to quickly abstract from what the data represents and to get lost in minutia. For example, as I started down the rabbit-hole of data discovery between the DATE_OF_BIRTH and AGE fields--I kept the query that confirmed these two didn't always align--I had to stop myself from finding several ways to justify a reason to drop one or both of these because of their misalignment. I needed to ground myself with a question about the value of these fields: This is donation data for organizations with information about the donors--does it matter how old they are? If it does, the fields can stay but they need to align. If not, drop them. Metadata kept me grounded to the reality of this data and, luckily, it was well documented metadata. This reiterates the importance of metadata. There were a few other tells about table composition that I liked exploring--were the right datatypes used (e.g. ZIPs as numbers drops crucial leading zeroes)? Were the parameters around them too restrictive or not restrictive enough (String fields allowed WAY too mcuh data)? I have a list of these observations in the notes.txt file, but the main lesson was that metadata can speak to the role it has in establishing and enforcing data quality. Lots to learn. I'm excited to continue working on this and other datasets. I was disappointed to not be able to wrap up some geocoding work on this in time.
+**/
