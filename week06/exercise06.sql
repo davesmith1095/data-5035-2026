34b5cf531b76b3551c4e30d0d1b9585592a80d71
CHEETAH <davesmith1095@gmail.com>
2026-02-19 19:03:06 +0000

    Adding draft unit tests

--- week06/exercise06.sql
+++ week06/exercise06.sql
@@ -21,17 +21,35 @@ SELECT *,
 -- Check for names missing capitalization
         CASE WHEN NAME != INITCAP(NAME) THEN 1 ELSE 0 END AS LOWER_NAMES,
 
+/*  Case 1: 'tim smith', 1
+    Case 2: 'Tim Smith', 0
+    Case 3: 'JIM SMITH', 0
+*/
+
 -- Check for short ZIPs (truncated 0s biproduct of number datatype) 
         CASE WHEN LENGTH(ZIP) < 5 THEN 1 ELSE 0 END AS SHORT_ZIP,
 
--- Case 1: LENGTH(ZIP) = 3
+/*  Case 1: '0054', 1
+    Case 2: '00592', 0
+    Case 3: '630318501', 0
+*/
+
 
 -- Check where N/A used, nulls already in Category
         CASE WHEN (CATEGORY = 'N/A' OR CATEGORY IS NULL) THEN 1 ELSE 0 END AS NULL_CATS,
 
+/*  Case 1: Category = 'N/A', 1,
+    Case 2: Category = NULL, 1,
+    Case 3: Category = 'Healthcare', 0
+*/
+
 -- Check phone length after all characters removed. > 10 is int'l or has ext.
         CASE WHEN LENGTH(REGEXP_REPLACE(PHONE, '[^0-9]', ''))>10 THEN 1 ELSE 0 END AS PHONE_LEN,
 -- From CTE which cites donations csv
-FROM YEAR_FIX
 
--- Case 
+/*  Case 1: 1-555, 0
+    Case 2: +1/111/555/5252, 0
+    Case 3: +125(5932)314-414
+*/
+
+FROM YEAR_FIX
