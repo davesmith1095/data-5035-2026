6d8ef754bf16ae153bdd2608488d19a539048100
CHEETAH <davesmith1095@gmail.com>
2026-03-29 13:53:46 +0000

    Finished Exercise B

--- exercise09_B.sql
+++ exercise09_B.sql
@@ -1,6 +1,6 @@
 -- Set role and schema
--- USE ROLE CHEETAH_DATA5035_ROLE;
--- USE SCHEMA DATA5035.CHEETAH;
+USE ROLE CHEETAH_DATA5035_ROLE;
+USE SCHEMA DATA5035.CHEETAH;
 /******************************************************************************
  * HEALTHCARE DATA - SQL EXERCISES
  ******************************************************************************/
@@ -33,3 +33,71 @@
 -- Business Description: Show visits handled by cardiology providers
 -- Columns Returned: patient name | provider name | visit_date
 -- Notes: Filter after join
+
+CREATE TABLE IF NOT EXISTS patients (
+    patient_id INT,
+    "name" STRING,
+    birth_year INT
+);
+
+CREATE TABLE IF NOT EXISTS visits (
+    visit_id INT,
+    patient_id INT,
+    visit_date DATE,
+    provider_id INT
+);
+
+CREATE TABLE IF NOT EXISTS "providers" (
+    provider_id INT,
+    provider_name STRING,
+    speciality STRING
+);
+
+INSERT INTO patients (patient_id, "name", birth_year) VALUES 
+    (1, 'John', 1980),
+    (2, 'Mary', 1975),
+    (3, 'Sam', 1990);
+
+INSERT INTO visits (visit_id, patient_id, visit_date, provider_id) VALUES 
+    (2001, 1, '2024-02-01', 10),
+    (2002, 2, '2024-02-03', 11);
+
+INSERT INTO "providers" (provider_id, provider_name, speciality) VALUES 
+    (10, 'Dr. Smith', 'Cardiology'),
+    (11, 'Dr. Lee', 'Primary Care'),
+    (12, 'Dr. Patel', 'Oncology');
+
+--Q1
+-- Using two INNER JOINs to bring together all detail data with a matching visit
+SELECT pa."name" AS "patient name", pr.provider_name AS "provider name", v.visit_date
+FROM visits v
+JOIN patients pa ON v.patient_id = pa.patient_id
+JOIN "providers" pr ON v.provider_id = pr.provider_id;
+
+--Q2
+-- Used RIGHT JOIN to bring in all patients then bring in visits (null or not)
+SELECT pa."name" AS "patient name", v.visit_id
+FROM visits v
+RIGHT JOIN patients pa ON v.patient_id = pa.patient_id
+
+
+--Q3
+-- Used RIGHT JOIN to bring in all providers then bring in visits (null or not)
+SELECT pr.provider_name AS "provider name", v.visit_id
+FROM visits v
+RIGHT JOIN "providers" pr ON v.provider_id = pr.provider_id
+
+--Q4
+-- Used RIGHT JOIN again to bring in all visits records per patients (null or not) but then specified with WHERE statement to ONLY include NULL. This shows patients w/o visits.
+SELECT pa."name" AS "patient name"
+FROM visits v
+RIGHT JOIN patients pa ON v.patient_id = pa.patient_id
+WHERE v.visit_id IS NULL
+
+--Q5
+-- Same as Q1 but with a WHERE clause to filter for Cardiology
+SELECT pa."name" AS "patient name", pr.provider_name AS "provider name", v.visit_date
+FROM visits v
+JOIN patients pa ON v.patient_id = pa.patient_id
+JOIN "providers" pr ON v.provider_id = pr.provider_id
+WHERE pr.speciality = 'Cardiology';
